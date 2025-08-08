1f8ea0d1aba8c0476227b620cff3c601157fc579
levimarkmuigai <levimarkmuigai@gmail.com>
2025-08-08 15:16:54 +0300

    Set up backend, frontend and database.

---
+++ backend/inventory-dash-backend/src/main/java/com/example/inventory_dash_backend/InventoryDashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventory_dash_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryDashBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryDashBackendApplication.class, args);
+	}
+
+}
---
+++ backend/inventory-dash-backend/src/test/java/com/example/inventory_dash_backend/InventoryDashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventory_dash_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryDashBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
