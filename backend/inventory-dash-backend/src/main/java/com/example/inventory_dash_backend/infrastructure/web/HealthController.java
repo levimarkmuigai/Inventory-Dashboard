73a4b35a169554902c3b89d443f2b4317009c7da
levimarkmuigai <levimarkmuigai@gmail.com>
2025-08-11 19:29:18 +0300

    Added health check for db.

--- backend/inventory-dash-backend/src/main/java/com/example/inventory_dash_backend/infrastructure/web/HealthController.java
+++ backend/inventory-dash-backend/src/main/java/com/example/inventory_dash_backend/infrastructure/web/HealthController.java
@@ -4,11 +4,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.persistence.PersistenceContext;
+
+import jakarta.persistence.EntityManager;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
 @RestController
 public class HealthController {
-    
+   
+    @PersistenceContext
+    private EntityManager entityManager;
     @GetMapping("/health")
-    public String health() {
-        return "ok";
+    public Map<String, String> health() {
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "UP");
+
+        try{
+            entityManager.createNativeQuery("Select 1").getSingleResult();
+            status.put("db", "Up");
+        }catch(Exception e){
+            status.put("db", "DOWN");
+        }
+        
+        return status;
     }
 }
